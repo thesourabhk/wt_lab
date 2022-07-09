eae6972d5d6ecd3312ca041b787908264b675512
Sourabh Kumar <skumardas267@gmail.com>
2022-07-09 23:41:10 +0530

    new labs added
    the missing labs are added

--- lab2b.js
+++ lab2b.js
@@ -1,14 +1,13 @@
 var a = parseInt(prompt("Enter n"));
 
 function fib() {
-    if(a<=0)
-    alert("Enter value greater than or equal to 1");
-    var str = "1";
-    for(var i=2; i<=a; i++){
-        str = str + ", " + i*i;
-    }
-    return str;
+  var a = prompt("Enter the number");
+  if (a <= 0) prompt("Enter value greater than or equal to 1");
+  var str = "1";
+  for (var i = 2; i <= a; i++) {
+    str = str + ", " + i * i;
+  }
+  return str;
 }
 
 alert(fib());
-
